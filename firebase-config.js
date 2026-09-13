ec30053d14854a38a86518caf68bebf2f7d9df2a
lbodybnkkk <elcomandoo121@gmail.com>
2026-09-13 20:07:19 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,64 +1,30 @@
 // firebase-config.js
-// تهيئة Firebase (Authentication + Realtime Database) لمنصة SAKANI-X
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-  updateProfile
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  push,
-  update,
-  remove,
-  onValue,
-  child,
-  query,
-  orderByChild,
-  equalTo
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getDatabase, ref, push, set, onValue, update, remove, get, child, query, orderByChild, equalTo, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDW1u0V41aKuphDC4FUdDsSZX7wUlb6twk", // ضع مفتاح الـ API الخاص بمشروعك هنا من إعدادات Firebase
-  authDomain: "cmd1-1c696.firebaseapp.com",
-  databaseURL: "https://cmd1-1c696-default-rtdb.firebaseio.com/",
-  projectId: "cmd1-1c696",
-  storageBucket: "cmd1-1c696.appspot.com",
-  messagingSenderId: "0000000000",
-  appId: "1:0000000000:web:0000000000000000000000"
+    apiKey: "AIzaSyDW1u0V41aKuphDC4FUdDsSZX7wUlb6twk", // ⚠️ أضف مفتاح API من Firebase Console
+    authDomain: "cmd1-1c696.firebaseapp.com",
+    databaseURL: "https://cmd1-1c696-default-rtdb.firebaseio.com/",
+    projectId: "cmd1-1c696",
+    storageBucket: "cmd1-1c696.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
-// تهيئة التطبيق
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-export {
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-  updateProfile,
-  ref,
-  set,
-  get,
-  push,
-  update,
-  remove,
-  onValue,
-  child,
-  query,
-  orderByChild,
-  equalTo
+// Providers
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+export { 
+    auth, db, ref, push, set, onValue, update, remove, get, child,
+    query, orderByChild, equalTo, serverTimestamp,
+    googleProvider, facebookProvider,
+    signInWithPopup, signOut, onAuthStateChanged,
+    createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile
 };
